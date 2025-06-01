4780c8afdd4c13b435325778f69a5392c1a24bad
Tanisha26g <tanishag.2608@gmail.com>
2025-06-01 13:55:34 +0530

    Some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import { FaFacebookF, FaTwitter, FaSkype, FaLinkedinIn } from "react-icons/fa";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="bg-dark text-white py-5">
       <Container>
         <Row className="gy-4">
-          {/* Club Info */}
           <Col md={3}>
-            <h2 className="text-primary fw-bold">Techno Clubs</h2>
+            <h2 className="text-primary fw-bold">E-Deewar</h2>
             <p className="text-light">
               We are a community of tech enthusiasts, working to build and share
               innovative tech solutions and projects.
@@ -25,10 +25,10 @@ const Footer = () => {
           <Col md={3}>
             <h5 className="fw-semibold">QUICK LINKS</h5>
             <ul className="list-unstyled text-light">
-              <li className="hover-effect">About Us</li>
-              <li className="hover-effect">Projects</li>
-              <li className="hover-effect">Blog</li>
-              <li className="hover-effect">Contact</li>
+              <li className="hover-effect" ><Link to="/about" className = "hover-effect" text-decoration-none>About Us</Link></li>
+              <li className="hover-effect"><Link to="/faqs" className = "hover-effect" text-decoration-none>FAQs</Link></li>
+              <li className="hover-effect"><Link to="/browse" className = "hover-effect" text-decoration-none>Listings</Link></li>
+              <li className="hover-effect"><Link to="/profile" className = "hover-effect" text-decoration-none>Profile</Link></li>
             </ul>
           </Col>
 
@@ -36,9 +36,9 @@ const Footer = () => {
           <Col md={3}>
             <h5 className="fw-semibold">CONTACT INFO</h5>
             <ul className="list-unstyled text-light">
-              <li>Techno Clubs, ABC University</li>
+              <li>E-Deewar, ABC University</li>
               <li>+1 234 567 890</li>
-              <li>📧 techxclub@abcuniversity.edu</li>
+              <li>📧 edeewar@abcuniversity.in</li>
             </ul>
           </Col>
 
@@ -61,7 +61,7 @@ const Footer = () => {
 
         {/* Footer Bottom */}
         <div className="text-center text-secondary mt-4 pt-3 border-top">
-          <p>© 2025 Techno Clubs. All Rights Reserved</p>
+          <p>© 2025 E-Deewar. All Rights Reserved</p>
           <div className="d-flex justify-content-center gap-3">
             <span className="hover-effect">Privacy Policy</span>
             <span className="hover-effect">Terms of Service</span>
